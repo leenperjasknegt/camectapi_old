3555c2e700b543abe7664a65de75277c71aafe0d
leenperjasknegt <leenknegtjasper@gmail.com>
2021-10-31 18:22:24 +0100

    v1.1

--- setup.sh
+++ setup.sh
@@ -4,36 +4,40 @@ echo Welcom to Camect API!
 echo
 echo Do not forget to turn off HTTPS traffic only in NX Witness server!
 echo Do not forget to accept the terms at https://camect.local
-ecgi
+echo
 echo Creating Camect API Service
 systemctl stop camectapi 
 systemctl stop http_api 
 cp /home/administrator/camect/camectapi.service /etc/systemd/system/camectapi.service
 cp /home/administrator/camect/http_api.service /etc/systemd/system/http_api.service
 systemctl daemon-reload
+sleep 1
 systemctl enable camectapi
 systemctl enable http_api
 
 
 echo NX server password:
+read varnxpassword
 if [ -z "$varnxpassword" ]
 then
       echo "Nothing changed"
 else
-sed -i "18c\nxpassword = '$varnxpassword'" /home/administrator/camect/camectapi.py
+sed -i "14c\nxpassword = '$varnxpassword'" /home/administrator/camect/camectapi.py
 sed -i "14c\nxpasswd = '$varnxpassword'" /home/administrator/camect/http_api.py
 fi
 
 echo Camect password:
+read varcamectpassword
 if [ -z "$varcamectpassword" ]
 then
       echo "Nothing changed"
 else
-sed -i "53c\home = camect.Home("camect.local:443", "admin", "$varnxpassword")" /home/administrator/camect/camectapi.py
-sed -i "98c\        '$varnxpassword'" /home/administrator/camect/camectapi.py
+sed -i "53c\home = camect.Home("camect.local:443", "admin", "$varcamectpassword")" /home/administrator/camect/camectapi.py
+sed -i "98c\        '$varcamectpassword'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 1:
+read varnxcamid1
 if [ -z "$varnxcamid1" ]
 then
       echo "Nothing changed"
@@ -42,6 +46,7 @@ sed -i "18c\cam1id = '$varnxcamid1'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 2:
+read varnxcamid2
 if [ -z "$varnxcamid2" ]
 then
       echo "Nothing changed"
@@ -50,6 +55,7 @@ sed -i "19c\cam2id = '$varnxcamid2'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 3:
+read varnxcamid3
 if [ -z "$varnxcamid3" ]
 then
       echo "Nothing changed"
@@ -58,6 +64,7 @@ sed -i "20c\cam3id = '$varnxcamid3'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 4:
+read varnxcamid4
 if [ -z "$varnxcamid4" ]
 then
       echo "Nothing changed"
@@ -66,6 +73,7 @@ sed -i "21c\cam4id = '$varnxcamid4'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 5:
+read varnxcamid5
 if [ -z "$varnxcamid5" ]
 then
       echo "Nothing changed"
@@ -74,6 +82,7 @@ sed -i "22c\cam5id = '$varnxcamid5'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 6:
+read varnxcamid6
 if [ -z "$varnxcamid6" ]
 then
       echo "Nothing changed"
@@ -82,6 +91,7 @@ sed -i "23c\cam6id = '$varnxcamid6'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 7:
+read varnxcamid7
 if [ -z "$varnxcamid7" ]
 then
       echo "Nothing changed"
@@ -90,6 +100,7 @@ sed -i "24c\cam7id = '$varnxcamid7'" /home/administrator/camect/camectapi.py
 fi
 
 echo Camera ID 8:
+read varnxcamid8
 if [ -z "$varnxcamid8" ]
 then
       echo "Nothing changed"
@@ -102,11 +113,11 @@ fi
 echo
 echo Starting Camect API
 
-
 systemctl start camectapi  
 systemctl start http_api
 
-echo Setting up NX Witness soft triggers
 
+echo Setting up NX Witness soft triggers
+sleep 5
 curl http://localhost:9000/disarm 
 curl http://localhost:9000/arm
